7e191c04ff03b4753cb1116ffd55fb2a4b12196c
DuaneMarshall <49883477+DuaneMarshall@users.noreply.github.com>
2025-11-19 12:40:38 -0700

    Add project files.

---
+++ Client-Server_demo/ServerApp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Client_Server_demo
+{
+    class ServerApp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Client_demo/ClientApp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Client_demo
+{
+    class ClientApp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
